e2a4587e8c8ae1598b604f55e6499394c6f0de11
LeKimThanh <lekimthanhx34@gmail.com>
2024-11-07 23:37:36 +0700

    video 29-Import/Export Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-
-
-//  () => {} 
-// React components are regular JavaScript funcition, but their names must start with a capital letter or won't work!
-//  component = htm + css + js
-const MyComponent = () => {
-  return (
-    <div>le kim thanh</div>
-  );
-}
+import MyComponent from './components/learn/MyComponent'
+import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -29,6 +20,8 @@ const App = () => {
       </div>
       <h1>Hello world</h1>
       <MyComponent />
+      <SecondComponent />
+      <ThirdComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,11 @@
+//  () => {} 
+// React components are regular JavaScript functions, but their names must start with a capital letter or won't work!
+//  component = htm + css + js
+
+const MyComponent = () => {
+    return (
+      <div>le kim thanh</div>
+    );
+  }
+
+  export default MyComponent;
