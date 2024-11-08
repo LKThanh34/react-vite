391ad8b80b023ccbb72b34379bed277c065c0680
LeKimThanh <lekimthanhx34@gmail.com>
2024-11-08 23:05:43 +0700

    video 30 - jsx-fragment

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,19 @@
 // React components are regular JavaScript functions, but their names must start with a capital letter or won't work!
 //  component = htm + css + js
 
+// fragment
+
+import './style.css';
+
 const MyComponent = () => {
     return (
-      <div>le kim thanh</div>
+      <>
+      <div>le kim thanh update</div>
+      <div className="child" style={
+        {borderRadius:"10px"}
+      } >child</div>
+
+      </>
     );
   }
 
